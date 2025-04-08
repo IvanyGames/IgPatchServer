04837d4a0272050fe630b06af395997e6d409d53
Danila <gamerxokks@gmail.com>
2025-04-08 22:03:09 +0700

    Adding support for ru_1.1.1.145

---
+++ IgPatch/Versions/Ru_1.1.1.145.h
@@ -0,0 +1,50 @@
+#define _Game_Unknown0_Address 0x0E294A0
+
+#define _Game_CSystem_IntializeEngineModule_Address 0x042F580
+#define _Game_CryCreateClassInstance_Address 0x042E830
+#define _Game_CBinaryXmlNode_getAttr_Address 0x0436D10
+#define _Game_CreateSystemInterface_Address 0x0428710
+
+#define _Game_ForceCryAISystemInitialization_Сondition false
+
+//IScriptSystem
+#define _Game_IScriptSystem_BeginPreCachedBuffer_Vtable_Offset 232//Not default
+#define _Game_IScriptSystem_PreCacheBuffer_Vtable_Offset 228//Not default
+#define _Game_IScriptSystem_CompileBuffer_Vtable_Offset 224//Not default
+
+//IEntitySystem
+#define _Game_IEntitySystem_AddSink_Vtable_Offset 92
+#define _Game_IEntitySystem_RemoveSink_Vtable_Offset 96
+#define _Game_IEntitySystem_GetIEntityPoolManager_Vtable_Offset 140
+#define _Game_IEntitySystem_GetEntityIterator_Vtable_Offset 68
+#define _Game_IEntitySystem_GetEntityFromPhysics_Vtable_Offset 88
+
+//ICryPak
+#define _Game_ICryPak_FindFirst_Vtable_Offset 216
+#define _Game_ICryPak_FindNext_Vtable_Offset 220
+#define _Game_ICryPak_FindClose_Vtable_Offset 224
+#define _Game_ICryPak_FClose_Vtable_Offset 184
+
+//ITimeOfDay
+#define _Game_ITimeOfDay_GetTime_Vtable_Offset 24
+
+//IXmlNode
+#define _Game_IXmlNode_getAttr_Vtable_Offset 104
+
+//SSystemGlobalEnvironment
+#define _Game_SSystemGlobalEnvironment_p3DEngine_Offset_Offset 4
+#define _Game_SSystemGlobalEnvironment_pScriptSystem_Offset_Offset 12
+#define _Game_SSystemGlobalEnvironment_pPhysicalWorld_Offset_Offset 16
+#define _Game_SSystemGlobalEnvironment_pCryPak_Offset_Offset 40
+#define _Game_SSystemGlobalEnvironment_pTimer_Offset_Offset 60 
+#define _Game_SSystemGlobalEnvironment_pGame_Offset_Offset 68
+#define _Game_SSystemGlobalEnvironment_pEntitySystem_Offset_Offset 76
+#define _Game_SSystemGlobalEnvironment_pConsole_Offset_Offset 80
+#define _Game_SSystemGlobalEnvironment_pSystem_Offset_Offset 88
+#define _Game_SSystemGlobalEnvironment_pLog_Offset_Offset 100
+#define _Game_SSystemGlobalEnvironment_pAISystem_Offset_Offset 96
+#define _Game_gEnv 0x1542900
+
+#define _Game_PathOther_EnableReadInDirectory_Address -1
+#define _Game_PathOther_Damage_Address -1
+#define _Game_PathOther_Damage_Address1 -1
